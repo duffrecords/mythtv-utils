d31cc30dade48765bfb9907c37205824214f1bf8
Ian Young <ian@duffrecords.com>
2016-12-01 17:15:35 -0800

    more refactoring and code cleanup

--- related.py
+++ related.py
@@ -5,6 +5,7 @@ from formatting import Formatting
 import sys
 import re
 
+# initialize some parameters
 titles_displayed = 21
 weight = {
     'director': 3.0,
@@ -12,7 +13,10 @@ weight = {
     'cast': 0.5,
     'rating': 1.0
 }
+categories = ['director', 'rating', 'year', 'cast']
+similar_movies = {}
 fmt = Formatting()
+db = Database()
 
 def get_random_movie_id(db):
     query = ("select intid from videometadata "
@@ -60,8 +64,6 @@ def get_list_index(listname, key, value):
 def escape_list(listname):
     return [re.sub("'", "\\'", s) for s in listname]
 
-db = Database()
-
 # start with user-specified movie or random movie if not specified
 if len(sys.argv) > 1:
     current_movie_title = ' '.join(sys.argv[1:])
@@ -75,15 +77,22 @@ if len(sys.argv) > 1:
 else:
     current_movie_id = get_random_movie_id(db)
 current_movie = get_movie_data(db, current_movie_id)
+
+# print table with formatting
 print fmt.reset + "Finding movies that are similar to:\n"
-header =  "%s (%s) %s" % (current_movie['title'], current_movie['year'], current_movie['director'])
+header =  "%s (%s) %s" % (current_movie['title'],
+                          current_movie['year'],
+                          current_movie['director'])
 print fmt.header + header + fmt.reset
 print fmt.ruler + ("-" * min(len(header), 80)) + fmt.reset
-print fmt.header + "Score" + fmt.reset + "   | " + fmt.header + "Movie" + fmt.reset
+print "%sScore%s   | %sMovie%s" % (fmt.header,
+                                   fmt.reset,
+                                   fmt.header,
+                                   fmt.reset)
 print fmt.ruler + ("-" * min(len(header), 80)) + fmt.reset
 
 # get list of all movies released in the same year
-movies_same_year = []
+similar_movies['year'] = []
 year = int(current_movie['year'])
 plus_or_minus_one = (str(year - 1), str(year + 1))
 plus_or_minus_two = "('" + str(year - 1) + "', '" + str(year + 1) + "')"
@@ -97,29 +106,29 @@ params = (current_movie['id'], current_movie['year'],
             year-1, year+1, year-2, year+2)
 results = db.run_query(query, params)
 for result in results:
-    movies_same_year.append(get_movie_data(db, str(result[0])))
-    movies_same_year[-1]['similarity'] += int(result[1])
+    similar_movies['year'].append(get_movie_data(db, str(result[0])))
+    similar_movies['year'][-1]['similarity'] += int(result[1])
 
 # get list of all movies by same director
-movies_same_director = []
+similar_movies['director'] = []
 query = ("select intid from videometadata "
         "where contenttype='MOVIE' and intid <> %s and director = %s")
 results = db.run_query(query, (current_movie['id'], current_movie['director']))
 for result in results:
-    movies_same_director.append(get_movie_data(db, str(result[0])))
-    movies_same_director[-1]['similarity'] += 1
+    similar_movies['director'].append(get_movie_data(db, str(result[0])))
+    similar_movies['director'][-1]['similarity'] += 1
 
 # get list of all movies with same rating
-movies_same_rating = []
+similar_movies['rating'] = []
 query = ("select intid from videometadata "
         "where contenttype='MOVIE' and intid <> %s and rating = %s")
 results = db.run_query(query, (current_movie['id'], current_movie['rating']))
 for result in results:
-    movies_same_rating.append(get_movie_data(db, str(result[0])))
-    movies_same_rating[-1]['similarity'] += 1
+    similar_movies['rating'].append(get_movie_data(db, str(result[0])))
+    similar_movies['rating'][-1]['similarity'] += 1
 
 # get list of all movies from same genres
-movies_same_genres = []
+similar_movies['genres'] = []
 placeholders = ', '.join(['%s'] * len(current_movie['genre']))
 query = ("select vm.intid, duplicates.totalCount "
         "from videometadata vm "
@@ -136,13 +145,12 @@ query = ("select vm.intid, duplicates.totalCount "
 params = [i for i in current_movie['genre']]
 params.append(current_movie['id'])
 results = db.run_query(query, params)
-#print "%s movies with the same genre" % len(results)
 for result in results:
-    movies_same_genres.append(get_movie_data(db, str(result[0])))
-    movies_same_genres[-1]['similarity'] += result[1]
+    similar_movies['genres'].append(get_movie_data(db, str(result[0])))
+    similar_movies['genres'][-1]['similarity'] += result[1]
 
 # get list of all movies featuring the same cast
-movies_same_cast = []
+similar_movies['cast'] = []
 placeholders = ', '.join(['%s'] * len(current_movie['cast']))
 query = ("select vm.intid, duplicates.totalCount "
         "from videometadata vm "
@@ -154,46 +162,29 @@ query = ("select vm.intid, duplicates.totalCount "
         "group by vm.intid) duplicates "
         "on vm.intid = duplicates.intid "
         "where vm.intid <> %s "
-        "order by duplicates.totalCount desc limit 1000;") % (placeholders, '%s')
+        "order by duplicates.totalCount desc "
+        "limit 1000;") % (placeholders, '%s')
 params = [i for i in current_movie['cast']]
 params.append(current_movie['id'])
 results = db.run_query(query, params)
 #print "%s movies with the same cast" % len(results)
 for result in results:
-    movies_same_cast.append(get_movie_data(db, str(result[0])))
-    movies_same_cast[-1]['similarity'] += result[1]
-    #print "%s cast: %s" % (movies_same_cast[-1]['title'], movies_same_cast[-1]['similarity'])
-    #if movies_same_cast[-1]['title'] == 'Quantum of Solace' or movies_same_cast[-1]['title'] == 'Casino Royale':
-        #print "%s has %s of the same actors" % (movies_same_cast[-1]['title'], movies_same_cast[-1]['similarity'])
+    similar_movies['cast'].append(get_movie_data(db, str(result[0])))
+    similar_movies['cast'][-1]['similarity'] += result[1]
 
 # merge lists into new list, ordered by similarity
 gallery_list = []
-for movie in movies_same_genres:
+for movie in similar_movies['genres']:
     #print "analyzing %s %s" % (movie['id'], movie['title'])
-    #if len(gallery_list) >= titles_displayed:
-    #    break
     gallery_list.append(movie)
-    m = get_list_index(movies_same_director, 'id', movie['id'])
-    if m:
-        increment = movies_same_director[m]['similarity'] * weight['director']
-        gallery_list[-1]['similarity'] += increment
-        #print "incrementing similarity of %s by %s because of director" % (increment, movie['title'])
-    m = get_list_index(movies_same_rating, 'id', movie['id'])
-    if m:
-        increment = movies_same_rating[m]['similarity'] * weight['rating']
-        gallery_list[-1]['similarity'] += increment
-        #print "incrementing similarity by %s because of rating" % increment
-    m = get_list_index(movies_same_year, 'id', movie['id'])
-    if m:
-        increment = movies_same_year[m]['similarity'] * weight['year']
-        gallery_list[-1]['similarity'] += increment
-        #print "incrementing similarity by %s because of year" % increment
-    m = get_list_index(movies_same_cast, 'id', movie['id'])
-    if m:
-        increment = movies_same_cast[m]['similarity'] * weight['cast']
-        gallery_list[-1]['similarity'] += increment
-        #print "incrementing similarity of %s by %s because of cast" % (increment, movie['title'])
+    for category in categories:
+        i = get_list_index(similar_movies[category], 'id', movie['id'])
+        if i:
+            inc = similar_movies[category][i]['similarity'] * weight[category]
+            gallery_list[-1]['similarity'] += inc
 
+# sort the final list and print in table form,
+# highlighting movies whose scores are above the mean
 gallery_list.sort(key=lambda x: x['similarity'], reverse=True)
 gallery_list = gallery_list[:titles_displayed]
 scores = [movie['similarity'] for movie in gallery_list]
@@ -203,4 +194,6 @@ for movie in gallery_list:
         text = fmt.boldtext
     else:
         text = fmt.text
-    print " %s%s\t%s|%s %s%s" % (text, movie['similarity'], fmt.ruler, text, movie['title'], fmt.reset)
+    print " %s%s\t%s|%s %s%s" % (text, movie['similarity'],
+                                fmt.ruler,
+                                text, movie['title'], fmt.reset)
